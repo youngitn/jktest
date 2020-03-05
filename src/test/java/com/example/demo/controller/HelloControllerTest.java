c8e7be79a046c7bd731ec8f5178e4f7a516ecca4
Yang Ting Cheng <youngtin1017@gmail.com>
2020-03-05 16:01:34 +0800

    testjk

---
+++ src/test/java/com/example/demo/controller/HelloControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class HelloControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
