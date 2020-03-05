7db6496af4fd9eb18818774345abd50e80d43778
Yang Ting Cheng <youngtin1017@gmail.com>
2020-03-05 16:19:59 +0800

    testJk 測試給他綠燈後

--- src/test/java/com/example/demo/controller/HelloControllerTest.java
+++ src/test/java/com/example/demo/controller/HelloControllerTest.java
@@ -1,15 +1,26 @@
 package com.example.demo.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class HelloControllerTest {
 
+	
+	
+	@Autowired
+	HelloController helloController;
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		
+		System.out.print(helloController.hello());
+		assertEquals("Hello",helloController.hello());
 	}
 
 }
