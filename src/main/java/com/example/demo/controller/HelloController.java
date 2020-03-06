01f55d7164b4ab0e2c38a65f74c8c82baa16d36f
Yang Ting Cheng <youngtin1017@gmail.com>
2020-03-06 15:54:58 +0800

    testGO

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 	@ResponseBody
-	@RequestMapping("/")
+	@RequestMapping("/") 
 	public String hello() {
 		return "Hello";
 	}
