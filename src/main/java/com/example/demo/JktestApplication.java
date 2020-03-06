39442264bf9e58d34d665041c86e532af89032b7
Yang Ting Cheng <youngtin1017@gmail.com>
2020-03-06 14:07:22 +0800

    Because without SpringBootServletInitializer Tomcat will consider it as normal application it will not consider as Spring boot application

--- src/main/java/com/example/demo/JktestApplication.java
+++ src/main/java/com/example/demo/JktestApplication.java
@@ -2,9 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JktestApplication {
+public class JktestApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JktestApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JktestApplication.class, args);
